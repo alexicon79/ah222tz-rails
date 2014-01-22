9539ec69ab055c60d804c029f24db1ba9cc224d7
Alexander Hall <ah@alxhall.se>
2014-01-22 23:37:37 +0100

    Adding model validations

--- AlxOnRails/app/models/quote.rb
+++ AlxOnRails/app/models/quote.rb
@@ -1,6 +1,6 @@
 class Quote < ActiveRecord::Base
-  attr_accessible :author, :rating, :text, :url, :year
+  attr_accessible :author, :rating, :text, :url
 
-	validates :text, presence: true
-
+	validates :text, presence: true, length: { minimum: 5, maximum: 800 }
+	validates :rating, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 10}
 end
